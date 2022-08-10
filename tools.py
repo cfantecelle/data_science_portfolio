be1c2b91952a3b178a387d4ac6892f0a50f0412e
Carlos Henrique Fantecelle <carloshdfc@gmail.com>
2022-08-10 09:42:38 -0300

    New tools.py with functions, updated churn analysis

---
+++ tools.py
@@ -0,0 +1,83 @@
+import pandas as pd
+
+def reformat_large_tick_values(tick_val, pos):
+    """
+    Turns large tick values (in the billions, 
+    millions and thousands) such as 4500 into 
+    4.5K and also appropriately turns 4000 
+    into 4K (no zero after the decimal).
+    """
+
+    if tick_val >= 1000000000:
+        val = round(tick_val/1000000000, 1)
+        new_tick_format = '{:}B'.format(val)
+    elif tick_val >= 1000000:
+        val = round(tick_val/1000000, 1)
+        new_tick_format = '{:}M'.format(val)
+    elif tick_val >= 1000:
+        val = round(tick_val/1000, 1)
+        new_tick_format = '{:}K'.format(val)
+    elif tick_val < 1000:
+        new_tick_format = round(tick_val, 1)
+    else:
+        new_tick_format = tick_val
+
+    # make new_tick_format into a string value
+    new_tick_format = str(new_tick_format)
+
+    # code below will keep 4.5M as is but change values such as 4.0M to 4M since that zero after the decimal isn't needed
+    index_of_decimal = new_tick_format.find(".")
+
+    if index_of_decimal != -1:
+        value_after_decimal = new_tick_format[index_of_decimal+1]
+        if value_after_decimal == "0":
+            # remove the 0 after the decimal point since it's not needed
+            new_tick_format = new_tick_format[0:index_of_decimal] + new_tick_format[index_of_decimal+2:]
+
+    return new_tick_format
+
+
+def uniqueValuesPerColumn(df):
+    """
+    Takes a dataframe and counts unique values in each
+    categorical column of the dataset.
+    """
+    
+    variables = df.select_dtypes('object').columns.to_list()
+
+    unique_values = []
+
+    for variable in variables:
+        unique_values.append(', '.join(df[variable].unique()))
+    
+    unique_df = pd.DataFrame({'Variable': variables,
+                              'Unique values': unique_values})
+    
+    return(unique_df)
+
+
+def classifyColumns(df):
+    """
+    Takes a dataframe and returns which columns
+    are numeric, binary categories or multiple
+    categories, in this order.
+    """
+
+    num_cols = df.select_dtypes(['int64', 'float64', 'int32', 'float32']).columns.to_list()
+    cat_cols = df.select_dtypes('object').columns.to_list()
+    bcat_cols = []
+    mcat_cols = []
+
+    for col in cat_cols:
+        if len(df[col].unique()) == 2:
+            bcat_cols.append(col)
+        elif len(df[col].unique()) > 2:
+            mcat_cols.append(col)
+
+    return num_cols, bcat_cols, mcat_cols
+
+
+
+
+
+
