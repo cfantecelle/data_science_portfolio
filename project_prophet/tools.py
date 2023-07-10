c207e544ac2475bd75eaeb2fc32140412ac8d213
Carlos Henrique Fantecelle <carloshdfc@gmail.com>
2023-07-10 18:42:11 -0300

    Updated Prophet analysis, started prophet

--- project_prophet/tools.py
+++ project_prophet/tools.py
@@ -19,6 +19,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 from datetime import datetime
+import textwrap
 
 
 ############################################################################################################################
@@ -60,3 +61,13 @@ def reformat_large_tick_values(tick_val, pos):
     #         new_tick_format = new_tick_format[0:index_of_decimal] + new_tick_format[index_of_decimal+2:]
 
     return new_tick_format
+
+
+# Defining some code to wrap labels (Code by: Ted Petrou @ Dunder Data)
+def wrap_labels(ax, width, break_long_words=False):
+    labels = []
+    for label in ax.get_xticklabels():
+        text = label.get_text()
+        labels.append(textwrap.fill(text, width=width,
+                      break_long_words=break_long_words))
+    ax.set_xticklabels(labels, rotation=0)
